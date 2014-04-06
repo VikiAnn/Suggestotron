cc76abc538bf8373f6037f48ec63c109fc2a8bf1
Victoria Harrod <victoria.harrod@gmail.com>
2014-04-06 00:41:32 +0000

    Added about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Suggestotron::Application.routes.draw do
+  get "about", to: 'about#index'
   resources :topics do
     member do
       post 'upvote'
@@ -16,7 +17,7 @@ Suggestotron::Application.routes.draw do
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
-
+  
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
